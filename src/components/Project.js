66c4ec117d745736684b877b215c61b84ebed6b1
anshum12345 <anshumdwivedi8@gmail.com>
2024-05-24 12:15:37 +0530

    second commit

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,32 +4,32 @@ import Card from "./Card";
 const Projects = () => {
   const projects = [
       {
-          imageSrc: "https://th.bing.com/th/id/R.7332e825525290cec9e0d30281db863f?rik=90ZUO%2bmAcDUyyw&riu=http%3a%2f%2fangrygamez.com%2fgames%2fimages%2fdoodle-jump_845x600.jpg&ehk=4jGG3sGw3e3CEEj3jIJ4Ar57llygtqiyeSuQN0Qe3r8%3d&risl=&pid=ImgRaw&r=0",
+          imageSrc: "https://th.bing.com/th/id/R.ad2fc33674600a750724b201cfebbbcd?rik=aQMasgmcNcqaDQ&riu=http%3a%2f%2fi.ytimg.com%2fvi%2f2nFDEugTliM%2fmaxresdefault.jpg&ehk=W8%2fr%2fOvVMCfRm68N9Z3HVi9fSZTDgpHEh0wN4RkExxQ%3d&risl=&pid=ImgRaw&r=0",
           title: "Doodle Jump",
-          tutorialLink: "https://youtu.be/pHFtOYU-a20",
-          demoLink: "https://imkennyyip.github.io/doodle-jump/",
-          repoLink: "https://github.com/ImKennyYip/doodle-jump"
+        //   tutorialLink: "https://youtu.be/pHFtOYU-a20",
+          demoLink: "https://doddle-game.vercel.app/",
+          repoLink: "https://github.com/anshum12345/Doddle-game"
       },
       {
-          imageSrc: "https://th.bing.com/th/id/R.7332e825525290cec9e0d30281db863f?rik=90ZUO%2bmAcDUyyw&riu=http%3a%2f%2fangrygamez.com%2fgames%2fimages%2fdoodle-jump_845x600.jpg&ehk=4jGG3sGw3e3CEEj3jIJ4Ar57llygtqiyeSuQN0Qe3r8%3d&risl=&pid=ImgRaw&r=0",
-          title: "Breakout",
-          tutorialLink: "https://youtu.be/EmhkLLWAZ8E",
-          demoLink: "https://imkennyyip.github.io/breakout/",
-          repoLink: "https://github.com/ImKennyYip/breakout"
+          imageSrc: "https://i1.wp.com/operationrainfall.com/wp-content/uploads/2014/01/candy-crush-saga.jpg",
+          title: "Candy Crush",
+        //   tutorialLink: "https://youtu.be/EmhkLLWAZ8E",
+          demoLink: "https://candy-crush-lemon.vercel.app/",
+          repoLink: "https://github.com/anshum12345/Candy-crush"
       },
       {
-          imageSrc: "https://th.bing.com/th/id/R.7332e825525290cec9e0d30281db863f?rik=90ZUO%2bmAcDUyyw&riu=http%3a%2f%2fangrygamez.com%2fgames%2fimages%2fdoodle-jump_845x600.jpg&ehk=4jGG3sGw3e3CEEj3jIJ4Ar57llygtqiyeSuQN0Qe3r8%3d&risl=&pid=ImgRaw&r=0",
-          title: "Pong",
-          tutorialLink: "https://youtu.be/LTtfoyHxAVU",
-          demoLink: "https://imkennyyip.github.io/pong/",
-          repoLink: "https://github.com/ImKennyYip/pong"
+          imageSrc: "https://th.bing.com/th/id/R.554c2649f84023fdbc1dbe9f8d4d9d64?rik=IhkGHF%2bqQ5%2b2Rg&riu=http%3a%2f%2fhdqwalls.com%2fdownload%2f1%2fflappy-birds-hd.jpg&ehk=m9tt6zm6YjXpYTqIvxA%2b6APWRMHLX6vahZJx%2buhM8LU%3d&risl=&pid=ImgRaw&r=0",
+          title: "Flappy Bird",
+        //   tutorialLink: "https://youtu.be/LTtfoyHxAVU",
+          demoLink: "https://flappy-bird-indol-two.vercel.app/",
+          repoLink: "https://github.com/anshum12345/flappy-bird"
       },
       {
           imageSrc: "https://th.bing.com/th/id/R.7332e825525290cec9e0d30281db863f?rik=90ZUO%2bmAcDUyyw&riu=http%3a%2f%2fangrygamez.com%2fgames%2fimages%2fdoodle-jump_845x600.jpg&ehk=4jGG3sGw3e3CEEj3jIJ4Ar57llygtqiyeSuQN0Qe3r8%3d&risl=&pid=ImgRaw&r=0",
           title: "Whac A Mole",
-          tutorialLink: "https://youtu.be/ej8SatOj3V4",
-          demoLink: "https://imkennyyip.github.io/whac-a-mole/",
-          repoLink: "https://github.com/ImKennyYip/whac-a-mole"
+        //   tutorialLink: "https://youtu.be/ej8SatOj3V4",
+          demoLink: "https://whack-a-mole-delta.vercel.app/",
+          repoLink: "https://github.com/anshum12345/whack-a-mole"
       },
       {
           imageSrc: "https://th.bing.com/th/id/R.7332e825525290cec9e0d30281db863f?rik=90ZUO%2bmAcDUyyw&riu=http%3a%2f%2fangrygamez.com%2fgames%2fimages%2fdoodle-jump_845x600.jpg&ehk=4jGG3sGw3e3CEEj3jIJ4Ar57llygtqiyeSuQN0Qe3r8%3d&risl=&pid=ImgRaw&r=0",
